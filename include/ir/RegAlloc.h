ba6cbb44121284ae8f88b9f14971e2042b3b0025
Tobias Stadler <mail@stadler-tobias.de>
2024-05-14 23:54:27 +0200

    [RegAlloc] remove stale linear-scan implementation

--- include/ir/RegAlloc.h
+++ include/ir/RegAlloc.h
@@ -5,9 +5,7 @@
 #include "ir/IRPass.h"
 #include "ir/RegLiveness.h"
 #include "support/Ranges.h"
-#include <algorithm>
 #include <cassert>
-#include <set>
 
 class RegAssignment {
 public:
@@ -48,72 +46,6 @@ public:
 
   PhysLiveSet phys;
 };
-
-/*
-class LinearRegAllocPass : public IRPass<Function> {
-
-  const char *name() override { return "RegAllocPass"; }
-
-  void run(Function &func, IRInfo<Function> &info) override {
-    alloc = RegAlloc();
-
-    RegTracking &regTrack = info.query<RegTracking>();
-    LiveIntervals &live = info.query<LiveIntervals>();
-
-    std::unordered_set<Reg> active;
-
-    const ArchRegClass &regClass = *info.getArch().getArchRegClass(1);
-    auto archRegs = info.getArch().getArchRegs();
-
-    std::vector<unsigned> regs(archRegs.back().reg + 1);
-    std::fill(regs.begin(), regs.end(), true);
-
-    for (Reg currReg = regTrack.begin(); currReg != regTrack.end();
-         currReg = currReg + 1) {
-      std::cout << "Picking: " << currReg << "\n";
-      auto intervalIt = live.intervals.find(currReg);
-      if (intervalIt == live.intervals.end())
-        continue;
-      LiveInterval &currInterval = intervalIt->second;
-      if (currInterval.empty())
-        continue;
-      for (auto it = active.begin(); it != active.end();) {
-        LiveInterval &otherInterval = live.intervals[*it];
-        if (otherInterval.getLast().to <= currInterval.getFirst().from) {
-          auto physRegIt = alloc.vRegToPhys.find(*it);
-          if (physRegIt != alloc.vRegToPhys.end()) {
-            std::cout << "Freeing: " << physRegIt->second << "\n";
-            regs[physRegIt->second] = true;
-            it = active.erase(it);
-            continue;
-          }
-        }
-        ++it;
-      }
-
-      for (auto *archReg : regClass.regs) {
-        if (regs[archReg->reg]) {
-          std::cout << "Assigning: " << archReg->reg << "\n";
-          regs[archReg->reg] = false;
-          alloc.vRegToPhys[currReg] = archReg->reg;
-          break;
-        }
-      }
-
-      active.insert(currReg);
-    }
-
-    info.publish(alloc);
-    info.preserve<RegTracking>();
-    info.preserve<LiveIntervals>();
-  }
-
-  void invalidate(IRInfo<Function> &info) override { info.retract<RegAlloc>(); }
-
-private:
-  RegAlloc alloc;
-};
-*/
 
 class RegRewritePass : public IRPass<Function> {
   const char *name() override { return "RegRewritePass"; }
@@ -315,7 +247,7 @@ class ColoringRegAllocPass : public IRPass<Function> {
     }
 
     std::cout << "no phys reg available\n";
-    regAssign.kind = RegAssignment::Assignment::NO_REG;
+    regAssign.assignNoReg();
   }
 
   void spillGlobal(Reg reg) {
